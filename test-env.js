a30678ab7de57c190f8676af3d9acf190f9fcae3
NickgotGitHub <sir.nsalmon@gmail.com>
2025-04-11 21:03:03 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Changes to be committed:
    	deleted:    test-env.js

--- test-env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-console.log('Environment variables check:');
-console.log('TWILIO_ACCOUNT_SID exists:', !!process.env.TWILIO_ACCOUNT_SID);
-console.log('TWILIO_AUTH_TOKEN exists:', !!process.env.TWILIO_AUTH_TOKEN);
-console.log('TWILIO_PHONE_NUMBER exists:', !!process.env.TWILIO_PHONE_NUMBER);
-console.log('TO_PHONE_NUMBER exists:', !!process.env.TO_PHONE_NUMBER); 
